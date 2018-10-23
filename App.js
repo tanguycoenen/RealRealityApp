e9adfd2d4a37a93f67c90d0f77213c6d03fc96f8
Tanguy Coenen <tanguy.coenen@gmail.com>
2018-10-23 23:44:14 +0200

    cleanup and styling

--- App.js
+++ App.js
@@ -71,7 +71,7 @@ export default class RealReality extends Component {
      .catch((error) =>{
        console.error(error);
      });
-  } 
+  }
 
   componentDidMount(){
     this.getGeoLocationPromise().then(
@@ -93,7 +93,6 @@ export default class RealReality extends Component {
 
   }
 
-
   render(){
 
     /*if(this.state.isLoading){
@@ -107,18 +106,10 @@ export default class RealReality extends Component {
     return(
 
        <KeyboardAvoidingView style={styles.container} behavior="padding">
-               <Text>RealRealityv0.2</Text>
-               <Text>Latitude: {this.state.latitude}</Text>
-               <Text>Longitude: {this.state.longitude}</Text>
+               <Text style={styles.title}>RealRealityApp_v0.2</Text>
+               <Text style={styles.text}>Latitude: {this.state.latitude}</Text>
+               <Text style={styles.text}>Longitude: {this.state.longitude}</Text>
                {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
-               <TouchableOpacity
-                 onPress={() => this.registerForPushNotifications()}
-                 style={styles.touchable}>
-                 <Text>Register me for notifications!</Text>
-               </TouchableOpacity>
-               <TouchableOpacity onPress={() => this.sendDelayedPushNotification()} style={styles.touchable}>
-                 <Text>Send me a notification!</Text>
-               </TouchableOpacity>
                <View>
                  <Text style={styles.text}>POIS</Text>
                  <FlatList
@@ -131,22 +122,6 @@ export default class RealReality extends Component {
                   keyExtractor={item => item.subject.value}
                  />
                </View>
-               {this.state.token ? (
-                 <View>
-                   <Text style={styles.text}>Token</Text>
-                   <TextInput
-                     style={styles.input}
-                     onChangeText={token => this.setState({ token })}
-                     value={this.state.token}
-                   />
-                 </View>
-               ) : null}
-               {this.state.notification ? (
-                 <View>
-                   <Text style={styles.text}>Last Notification:</Text>
-                   <Text style={styles.text}>{JSON.stringify(this.state.notification.data.message)}</Text>
-                 </View>
-               ) : null}
          </KeyboardAvoidingView>
 
     );
@@ -154,22 +129,18 @@ export default class RealReality extends Component {
 }
 const styles = StyleSheet.create({
   title: {
-    backgroundColor: '#0f0',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    flex: 1,
-    fontSize: 18,
-    padding: 8,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: '#ffffff',
   },
   text: {
-    backgroundColor: '#0f0',
     paddingBottom: 2,
     padding: 8,
+    color: '#ffffff',
   },
   container: {
-    backgroundColor: '#0f0',
+    backgroundColor: '#000000',
     flex: 1,
     paddingTop: 40,
   },
